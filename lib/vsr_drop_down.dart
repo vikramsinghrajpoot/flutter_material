fa9e71cf060b2f51e5e49fda1054b03d2ee7c9db
vikram <vikram_rajpoot@external.mckinsey.com>
2024-01-10 11:49:02 +0530

    Updated dropdown props

--- lib/vsr_drop_down.dart
+++ lib/vsr_drop_down.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'vsr_text.dart';
 
 class VSRDropDown extends StatefulWidget {
@@ -7,12 +6,32 @@ class VSRDropDown extends StatefulWidget {
   final String? title;
   final String? defaultValue;
   final EdgeInsets padding;
+  final EdgeInsets margin;
+  final String? hintText;
+  final Widget? userlineWidget;
+  final int elevation;
+  final double? height;
+  final double borderRadius;
+  final TextStyle? titleStyle;
+  final TextStyle? hintStyle;
+  final BoxBorder? border;
+  final bool? isExpanded;
   const VSRDropDown(
       {key,
       this.options,
       this.title,
       this.defaultValue,
-      this.padding = EdgeInsets.zero})
+      this.padding = EdgeInsets.zero,
+      this.margin = EdgeInsets.zero,
+      this.hintText = "",
+      this.userlineWidget,
+      this.elevation = 0,
+      this.height = 35,
+      this.borderRadius = 5,
+      this.hintStyle,
+      this.titleStyle,
+      this.border,
+      this.isExpanded = true})
       : super(key: key);
 
   @override
@@ -39,26 +58,31 @@ class _VSRDropDownState extends State<VSRDropDown> {
             VSRText(
               widget.title ?? "",
               padding: const EdgeInsets.only(left: 7, bottom: 3),
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              style: widget.titleStyle ??
+                  const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
             ),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            margin: const EdgeInsets.symmetric(horizontal: 5),
+            padding: widget.padding,
+            margin: widget.margin,
             decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey.shade400),
-                borderRadius: BorderRadius.circular(5)),
+                border:
+                    widget.border ?? Border.all(color: Colors.grey.shade400),
+                borderRadius: BorderRadius.circular(widget.borderRadius)),
             child: SizedBox(
-              height: 37,
+              height: widget.height,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
                     child: DropdownButton<String>(
-                      underline: SizedBox(),
-                      elevation: 0,
-                      hint: const Text('Select property type'),
-                      isExpanded: true,
+                      underline: widget.userlineWidget,
+                      elevation: widget.elevation,
+                      hint: VSRText(
+                        widget.hintText,
+                        style: widget.hintStyle,
+                      ),
+                      isExpanded: widget.isExpanded!,
                       value: selectedOption,
                       onChanged: (String? newValue) {
                         setState(() {
@@ -68,7 +92,7 @@ class _VSRDropDownState extends State<VSRDropDown> {
                       items: widget.options?.map((String option) {
                         return DropdownMenuItem<String>(
                           value: option,
-                          child: Text(option),
+                          child: VSRText(option),
                         );
                       }).toList(),
                     ),
